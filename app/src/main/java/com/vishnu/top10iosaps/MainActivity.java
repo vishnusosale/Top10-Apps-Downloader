0f506a039be55d1275386ed937accc3608299714
Vishnu Sosale <sosalevishnu@gmail.com>
2015-06-29 01:13:32 +0530

    GET response success

--- app/src/main/java/com/vishnu/top10iosaps/MainActivity.java
+++ app/src/main/java/com/vishnu/top10iosaps/MainActivity.java
@@ -1,24 +1,33 @@
 package com.vishnu.top10iosaps;
 
+import android.app.Activity;
 import android.os.AsyncTask;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
+
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView  = (TextView)findViewById(R.id.textView);
+
+        new DownloadData().execute("http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topfreeapplications/limit=10/xml");
     }
 
     @Override
@@ -43,60 +52,84 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    private class DownloadData extends AsyncTask<String,Void,String>
-    {
+    private String downloadXML(String url) throws IOException {
+
+        // how many characters at a time we are gonna download from file
+        final int BUFFER_SIZE = 2000;
+
+        InputStream inputStream = null;
+        String xmlContents = "";
+
+
+        try {
+            URL mUrl = new URL(url);
+            HttpURLConnection httpURLConnection = (HttpURLConnection) mUrl.openConnection();
+            httpURLConnection.setReadTimeout(10000);
+            httpURLConnection.setConnectTimeout(15000);
+            httpURLConnection.setRequestMethod("GET");
+            ;
+            httpURLConnection.setDoInput(true);
+
+            int response = httpURLConnection.getResponseCode();
+            Log.d("ResponseCode", response + "");
+
+            inputStream = httpURLConnection.getInputStream();
+
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+            int charRead;
+            char[] inputBuffer = new char[BUFFER_SIZE];
+
+            try {
+                while ((charRead = inputStreamReader.read(inputBuffer)) > 0) {
+                    String readString = String.copyValueOf(inputBuffer, 0, charRead);
+                    xmlContents += readString;
+                    inputBuffer = new char[BUFFER_SIZE];
+                }
+
+                return xmlContents;
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            }
+
+
+        } catch (Exception e) {
+            Log.e("downLoadXML", e.toString());
+        } finally {
+            if (inputStream != null) {
+                inputStream.close();
+            }
+        }
+
+
+        return "";
+    }
+
+
+    private class DownloadData extends AsyncTask<String, Void, String> {
 
         String xmlData;
+
+        @Override
+        protected void onPostExecute(String s) {
+
+            Log.d("OnPostExecute", xmlData);
+            textView.setText(xmlData);
+        }
 
         @Override
         protected String doInBackground(String... urls) {
             try {
 
-                data = downloadXML(urls[0]);
+                xmlData = downloadXML(urls[0]);
 
-            }catch (Exception e)
-            {
+            } catch (Exception e) {
                 Log.e("DownloadData", e.toString());
                 return "Cannot download xml file";
             }
 
             return "";
         }
-    }
-
-    private String downloadXML(String url) throws IOException{
-
-        // how many characters at a time we are gonna download from file
-        final int BUFFER_SIZE  = 2000;
-
-        InputStream inputStream = null;
-        String xmlContents;
-
-
-        try
-        {
-            URL mUrl = new URL(url);
-            HttpURLConnection httpURLConnection = (HttpURLConnection) mUrl.openConnection();
-            httpURLConnection.setReadTimeout(10000);
-            httpURLConnection.setConnectTimeout(15000);
-            httpURLConnection.setRequestMethod("GET");;
-            httpURLConnection.setDoInput(true);
-
-            int response = httpURLConnection.getResponseCode();
-            Log.d("ResponseCode", response+"");
-        }
-        catch (Exception e)
-        {
-            Log.e("downLoadXML", e.toString());
-        }
-        finally {
-            if (inputStream!=null)
-            {
-                    inputStream.close();
-            }
-        }
-
-
-        return "";
     }
 }
